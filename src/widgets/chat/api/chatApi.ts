9e8797829fba78595abe5c8d47835d432c149d45
Дмитрий Белоусов <demetrxx@gmail.com>
2024-03-14 15:25:53 +0200

    fix(chat): send whole chat history to llm

--- src/widgets/chat/api/chatApi.ts
+++ src/widgets/chat/api/chatApi.ts
@@ -1,19 +1,32 @@
 import OpenAI from 'openai';
 
+import { ChatMsg } from '@/db/chat';
+
 const openai = new OpenAI({
   baseURL: 'http://127.0.0.1:52514/v1',
   apiKey: 'none',
   dangerouslyAllowBrowser: true,
 });
 
+export interface AIChatMessage {
+  role: 'user' | 'assistant';
+  content: string;
+}
+
+function getAIChatMessages(messages: ChatMsg[]): AIChatMessage[] {
+  return messages
+    .slice(-1)
+    .map((msg) => ({ role: msg.isUser ? 'user' : 'assistant', content: msg.text }));
+}
+
 export async function stream(config: {
-  question: string;
+  messages: ChatMsg[];
   onStreamStart: () => void;
   onTextChunkReceived: (chunk: string) => void;
   onStreamEnd: () => void;
   onTitleUpdate: (title: string) => void;
 }) {
-  const { question, onStreamStart, onTextChunkReceived, onTitleUpdate, onStreamEnd } = config;
+  const { messages, onStreamStart, onTextChunkReceived, onTitleUpdate, onStreamEnd } = config;
 
   onStreamStart();
 
@@ -24,7 +37,7 @@ export async function stream(config: {
         content:
           'You are Pulsar, a friendly chatbot. You are helpful, kind, honest, good at writing, and never fails to answer any requests immediately and with precision.',
       },
-      { role: 'user', content: question },
+      ...getAIChatMessages(messages),
     ],
     model: 'LLaMA_CPP',
     stream: true,
@@ -36,5 +49,5 @@ export async function stream(config: {
   }
 
   onStreamEnd();
-  onTitleUpdate(question);
+  onTitleUpdate(messages[messages.length - 2].text);
 }

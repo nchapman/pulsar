a300695e85a99f44c4112cc15a7506d14101586a
Oscar Franco <ospfranco@gmail.com>
2024-03-19 13:36:10 +0100

    Update version

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -1,23 +1,21 @@
 // print the working directory
-console.log('Current directory:', process.cwd());
-
 const {execSync} = require('child_process');
 const fs = require('fs');
 const path = require('path');
+
+function bumpPatchVersion(version) {
+  const parts = version.split('.');
+  const patch = parseInt(parts[2], 10) + 1;
+  return `${parts[0]}.${parts[1]}.${patch}`;
+}
+
 
 try {
   const tauriJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../src-tauri/tauri.conf.json'), 'utf8'));
 
   // update the version
-  function bumpPatchVersion(version) {
-    const parts = version.split('.');
-    const patch = parseInt(parts[2], 10) + 1;
-    return `${parts[0]}.${parts[1]}.${patch}`;
-  }
-  
-  
-  
-  console.log('read tauri conf', tauriJson.package.version);
+
+  console.log('Current version', tauriJson.package.version);
   
   // Example usage:
   const currentVersion = tauriJson.package.version;
@@ -27,7 +25,7 @@ try {
   tauriJson.version = newVersion;
   
   // Write the updated tauriJson back to the file
-  fs.writeFileSync('./src-tauri/tauri.conf.json', JSON.stringify(tauriJson, null, 2));
+  fs.writeFileSync(path.resolve(__dirname, '../src-tauri/tauri.conf.json'), JSON.stringify(tauriJson, null, 2));
   
   console.log('Version updated successfully!');
   

0e5cff1887ddcd2e704dfa645fe983622938b61d
Oscar Franco <ospfranco@gmail.com>
2024-03-19 13:02:42 +0100

    Update manifest

--- scripts/updateManifest.cjs
+++ scripts/updateManifest.cjs
@@ -1,6 +1,9 @@
 const {execSync} = require('child_process');
 // This script is meant to read the manifest from a gist, update it and the commit it back to the gist
 
+// Remove the manifest folder if it exists
+execSync('rm -rf manifest');
+
 // Clone the repository
 execSync('git clone https://gist.github.com/ospfranco/2399e4a47b54e82049dd23741e5d9582 manifest');
 
@@ -8,7 +11,7 @@ execSync('git clone https://gist.github.com/ospfranco/2399e4a47b54e82049dd23741e
 const manifest = require('./manifest/manifest.json');
 
 // Update the manifest
-manifest.version = '1.0.0';
+manifest.version = '1.0.1';
 
 // Write the manifest
 require('fs').writeFileSync('./manifest/manifest.json', JSON.stringify(manifest, null, 2));
@@ -17,5 +20,7 @@ require('fs').writeFileSync('./manifest/manifest.json', JSON.stringify(manifest,
 execSync('git add .');
 
 execSync('git commit -m "Update manifest"');
+
+execSync('git push');
 
 

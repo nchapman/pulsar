1d2aa6eb690bb2758b3b1b2680026b9f6dbac1c1
Oscar Franco <ospfranco@gmail.com>
2024-04-10 12:50:59 +0200

    Move modelExists to check for appData dit

--- src/entities/model/lib/modelExists.ts
+++ src/entities/model/lib/modelExists.ts
@@ -3,7 +3,7 @@ import { BaseDirectory, readDir } from '@tauri-apps/api/fs';
 import { AIModelName, models, MODELS_DIR_NAME } from '@/constants';
 
 export async function modelExists(model?: AIModelName) {
-  const entries = await readDir(MODELS_DIR_NAME, { dir: BaseDirectory.AppLocalData });
+  const entries = await readDir(MODELS_DIR_NAME, { dir: BaseDirectory.AppData });
 
   if (!model) {
     return entries.length > 0;

4792748423f3bbd160ea76f0be198a5108ae378c
demetrxx <demetrxx@gmail.com>
2024-04-21 11:44:01 +0300

    fix(models): fix path reading

--- src/entities/model/hooks/useModelReady.ts
+++ src/entities/model/hooks/useModelReady.ts
@@ -2,6 +2,7 @@ import { useUnit } from 'effector-react';
 import { useCallback, useEffect, useState } from 'preact/hooks';
 
 import { AIModel } from '@/entities/model';
+import { useLog } from '@/shared/lib/hooks';
 
 import { $llama } from '../consts/llama.ts';
 import { hasModel } from '../lib/hasModel.ts';
@@ -9,6 +10,8 @@ import { hasModel } from '../lib/hasModel.ts';
 export function useModelReady(modelName: AIModel) {
   const [ready, setReady] = useState(true);
   const llama = useUnit($llama);
+
+  useLog({ modelName, ready }, 'useModelReady');
 
   useEffect(() => {
     hasModel(modelName).then(setReady);

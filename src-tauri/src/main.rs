4e181c8b24d9ca5d6b15717532a1de87afc848c1
ospfranco <ospfranco@gmail.com>
2024-04-30 10:24:24 +0200

    Add a way to open app data dir

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,24 +7,11 @@ mod upload;
 use log::info;
 use tauri_plugin_log::LogTarget;
 
-fn setup<R>(app: &mut tauri::App<R>) -> Result<(), Box<dyn std::error::Error>>
-where
-    R: tauri::Runtime,
-{
-    let app_data_dir = app.handle().path_resolver().app_data_dir();
-    info!(
-        "App data dir: \x1b[34m{}\x1b[0m",
-        app_data_dir.unwrap().to_str().unwrap()
-    );
-    Ok(())
-}
-
 fn main() {
     use log::LevelFilter;
     use tauri_plugin_log::fern::colors::ColoredLevelConfig;
 
     tauri::Builder::default()
-        .setup(setup)
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets([LogTarget::Stdout])
--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import { memo } from 'preact/compat';
 import { classNames } from '@/shared/lib/func';
 import { LeftPanel } from '@/shared/ui';
 
+import { appDataDir } from '@tauri-apps/api/path';
+import { open as openPath } from '@tauri-apps/api/shell';
 import { ChatHistory } from '../ChatHistory/ChatHistory.tsx';
 import { SidebarFooter } from '../SidebarFooter/SidebarFooter.tsx';
 import s from './Sidebar.module.scss';
@@ -15,6 +17,11 @@ interface Props {
 export const Sidebar = memo((props: Props) => {
   const { className, open } = props;
 
+  const openAppData = async () => {
+    const appDataDirPath = await appDataDir();
+    openPath(appDataDirPath);
+  };
+
   return (
     <LeftPanel
       open={open}
@@ -23,6 +30,12 @@ export const Sidebar = memo((props: Props) => {
     >
       <ChatHistory className={s.chatHistory} />
       <SidebarFooter className={s.sidebarFooter} />
+      {/* @ts-ignore */}
+      {process.env.NODE_ENV === 'development' && (
+        <button className={s.debug} onClick={openAppData}>
+          Open App Data
+        </button>
+      )}
     </LeftPanel>
   );
 });

d69ba008ddcf1da54179a82a1a04ee09dd31499e
ospfranco <ospfranco@gmail.com>
2024-05-03 19:50:45 +0200

    Update nebula

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,10 @@
 fn main() {
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lomp,-L/opt/homebrew/opt/libomp/lib");
-    } else if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lstdc++");
-    }
+    // Needed flags for sqlite-vss
+    // if cfg!(target_os = "macos") {
+    //     println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lomp,-L/opt/homebrew/opt/libomp/lib");
+    // } else if cfg!(target_os = "linux") {
+    //     println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lstdc++");
+    // }
 
     tauri_build::build()
 }

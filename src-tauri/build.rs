5eff7d5fc3c38b4b4e63ddeaf13f5b11c38243a8
ospfranco <ospfranco@gmail.com>
2024-05-09 17:19:10 +0200

    Remove libomp path

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,11 @@
 fn main() {
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lomp,-L/opt/homebrew/opt/libomp/lib");
-    } else if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lstdc++");
-    }
+    // Re-add when sqlite-vss or sqlite-vec is installed
+    // Also add the install step in the CI workflows
+    // if cfg!(target_os = "macos") {
+    //     println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lomp,-L/opt/homebrew/opt/libomp/lib");
+    // } else if cfg!(target_os = "linux") {
+    //     println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup,-lstdc++");
+    // }
 
     tauri_build::build()
 }

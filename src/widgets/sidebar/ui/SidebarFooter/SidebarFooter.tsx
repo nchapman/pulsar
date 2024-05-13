28d73284d0f11b25ea7a5fca521dbca9fa4bbbc2
ospfranco <ospfranco@gmail.com>
2024-05-13 09:38:07 +0200

    First approach to get the gguf models from hugging face

--- src/widgets/sidebar/ui/SidebarFooter/SidebarFooter.tsx
+++ src/widgets/sidebar/ui/SidebarFooter/SidebarFooter.tsx
@@ -2,7 +2,12 @@ import { appDataDir } from '@tauri-apps/api/path';
 import { open as openPath } from '@tauri-apps/api/shell';
 import { memo } from 'preact/compat';
 
+import {
+  fetchHuggingFaceModel,
+  searchHuggingFaceModel,
+} from '@/features/hugging-face-search/hugging-face-search';
 import { classNames } from '@/shared/lib/func';
+import { logi } from '@/shared/lib/Logger';
 import { Button } from '@/shared/ui';
 import { openSettingsModal, SettingsModal } from '@/widgets/settings';
 
@@ -24,9 +29,24 @@ export const SidebarFooter = memo((props: Props) => {
     <div className={classNames(s.sidebarFooter, [className])}>
       {/* @ts-ignore */}
       {process.env.NODE_ENV === 'development' && (
-        <Button className={s.btn} onClick={openAppData} variant="clear">
-          Open App Data
-        </Button>
+        <>
+          <Button className={s.btn} onClick={openAppData} variant="clear">
+            Open App Data
+          </Button>
+          <Button
+            className={s.btn}
+            onClick={async () => {
+              const searchResults = await searchHuggingFaceModel('cjpais');
+
+              const model = await fetchHuggingFaceModel(searchResults[0].modelId);
+
+              logi('Hugging face model', JSON.stringify(model, null, 2));
+            }}
+            variant="clear"
+          >
+            Hugging face test
+          </Button>
+        </>
       )}
 
       <Button className={s.btn} onClick={openSettingsModal} variant="secondary">

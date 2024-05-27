cda9a5fc44f72dcc75dfb52cea642a1f2c5227b2
ospfranco <ospfranco@gmail.com>
2024-05-27 10:40:25 +0200

    Bind drop_all function

--- src/entities/model/nebula/Nebula.ts
+++ src/entities/model/nebula/Nebula.ts
@@ -4,5 +4,9 @@ export class Nebula {
   public static async getLoadedModels() {
     return invoke<string>('plugin:nebula|get_loaded_models');
   }
+
+  public static async dropAll() {
+    return invoke('plugin:nebula|drop_all');
+  }
 }
 

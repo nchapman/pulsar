83e726745a8596874cabd03bc3c40e6cd41800e8
Дмитрий Белоусов <demetrxx@gmail.com>
2024-06-05 10:04:29 +0300

    fix(onboarding): force dark theme

--- src/entities/model/ui/InitialModelDownload/InitialModelDownload.tsx
+++ src/entities/model/ui/InitialModelDownload/InitialModelDownload.tsx
@@ -1,10 +1,11 @@
 import { useStoreMap } from 'effector-react';
-import { memo } from 'preact/compat';
+import { memo, useLayoutEffect } from 'preact/compat';
 
 import { DEFAULT_LLM, ModelCard } from '@/entities/model';
 import DownloadIcon from '@/shared/assets/icons/download.svg';
 import { classNames } from '@/shared/lib/func';
 import { useToggle } from '@/shared/lib/hooks';
+import { changeTheme } from '@/shared/theme';
 import { Button, Icon, Progress, Text } from '@/shared/ui';
 
 import { $modelsDownload, downloadModelEff } from '../../model/manage-models-model.ts';
@@ -19,10 +20,18 @@ const model = DEFAULT_LLM;
 export const InitialModelDownload = memo((props: Props) => {
   const { className } = props;
   const { on: pause, off: resume, isOn: isPaused } = useToggle();
+  const { on: startDownload, isOn: isDownloading } = useToggle();
 
   const downloadInfo = useStoreMap($modelsDownload, (s) => s[model]);
 
-  const handleModelDownload = () => downloadModelEff(model);
+  useLayoutEffect(() => {
+    changeTheme('dark');
+  }, []);
+
+  const handleModelDownload = () => {
+    startDownload();
+    downloadModelEff(model);
+  };
 
   return (
     <div className={classNames(s.initialModelDownload, [className])}>
@@ -41,7 +50,7 @@ export const InitialModelDownload = memo((props: Props) => {
             percent={downloadInfo?.percent}
           />
         ) : (
-          <Button onClick={handleModelDownload} variant="primary">
+          <Button onClick={handleModelDownload} variant="primary" loading={isDownloading}>
             <Icon svg={DownloadIcon} />
             Download model
           </Button>

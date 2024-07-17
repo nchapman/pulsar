83932560d703f08cad8d3bf06dd47e0b4f814869
ospfranco <ospfranco@gmail.com>
2024-07-17 14:27:59 -0500

    Release 0.0.39

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
@@ -9,39 +9,44 @@ function bumpPatchVersion(version) {
   return `${parts[0]}.${parts[1]}.${patch}`;
 }
 
-
 try {
-  const tauriJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../src-tauri/tauri.conf.json'), 'utf8'));
+  const tauriJson = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../src-tauri/tauri.conf.json'), 'utf8')
+  );
 
   console.log('🟦 Current version', tauriJson.package.version);
-  
+
   // Example usage:
   const currentVersion = tauriJson.package.version;
   const newVersion = bumpPatchVersion(currentVersion);
   // Update the version in tauriJson
   tauriJson.package.version = newVersion;
-  
+
   // Write the updated tauriJson back to the file
-  fs.writeFileSync(path.resolve(__dirname, '../src-tauri/tauri.conf.json'), JSON.stringify(tauriJson, null, 2));
+  fs.writeFileSync(
+    path.resolve(__dirname, '../src-tauri/tauri.conf.json'),
+    JSON.stringify(tauriJson, null, 2)
+  );
 
   // log git status
   const status = execSync('git status').toString();
 
-  if(status.includes('nothing to commit, working tree clean')) {
+  if (status.includes('nothing to commit, working tree clean')) {
     console.log('🟥 Nothing to commit');
     return;
   }
 
   console.log(`🟢 Updated to ${tauriJson.package.version}`);
 
-  execSync(`git checkout -b ${newVersion}`);
+  // execSync(`git checkout -b ${newVersion}`);
 
   execSync('git add .');
-  execSync('git commit -m "Update version"');
-  execSync(`git push --set-upstream origin ${newVersion}`);
+  execSync(`git commit -m "Release ${newVersion}"`);
+  execSync(`git push`);
 
-  execSync(`git tag -a v${  newVersion  } -m "Release version ${  newVersion  }"`);
-  
+  execSync(`git tag -a v${newVersion} -m "Release ${newVersion}"`);
+  execSync(`git push --tags`);
 } catch (error) {
   console.error('Error:', error);
 }
+

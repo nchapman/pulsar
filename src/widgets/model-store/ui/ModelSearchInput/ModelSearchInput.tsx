502ea6a3739050d2eccb283d87c1a9af30233603
Дмитрий Белоусов <demetrxx@gmail.com>
2024-07-21 20:03:29 +0300

    feat(model-store): add clear btn to search form

--- src/widgets/model-store/ui/ModelSearchInput/ModelSearchInput.tsx
+++ src/widgets/model-store/ui/ModelSearchInput/ModelSearchInput.tsx
@@ -1,10 +1,12 @@
 import { useUnit } from 'effector-react';
 import { memo } from 'preact/compat';
-import { useCallback } from 'preact/hooks';
+import { useCallback, useRef } from 'preact/hooks';
 
 import GoIcon from '@/shared/assets/icons/arrow-right.svg';
+import CrossIcon from '@/shared/assets/icons/close.svg';
 import LensIcon from '@/shared/assets/icons/search.svg';
 import { classNames } from '@/shared/lib/func';
+import { useToggle } from '@/shared/lib/hooks';
 import { Button, Icon, Input } from '@/shared/ui';
 
 import {
@@ -22,18 +24,31 @@ const placeholder = 'Search for models on Hugging Face';
 
 export const ModelSearchInput = memo((props: Props) => {
   const { className } = props;
+
   const value = useUnit($modelStoreState.searchValue);
   const isLoading = useUnit(fetchHFModels.pending);
+
+  const inputRef = useRef<any>();
+
+  const { isOn: isFocused, on: onFocusIn, off: onFocusOut } = useToggle();
 
   const handleSubmit = useCallback((e: any) => {
     e.preventDefault();
     modelStoreEvents.searchHF();
   }, []);
 
+  const handleResetInput = useCallback(() => {
+    modelStoreEvents.setSearchValue('');
+    inputRef.current.focus();
+  }, []);
+
   return (
     <form onSubmit={handleSubmit} className={classNames(s.modelSearchInput, [className])}>
       <Icon svg={LensIcon} className={s.icon} />
       <Input
+        ref={inputRef}
+        onFocusIn={onFocusIn}
+        onFocusOut={onFocusOut}
         autofocus
         placeholder={placeholder}
         className={s.input}
@@ -41,14 +56,26 @@ export const ModelSearchInput = memo((props: Props) => {
         disabled={isLoading}
         onChange={modelStoreEvents.setSearchValue}
       />
-      <Button
-        disabled={isLoading}
-        loading={isLoading}
-        variant="clear"
-        type="submit"
-        className={s.go}
-        icon={GoIcon}
-      />
+      {isFocused && value && (
+        <Button
+          disabled={isLoading}
+          loading={isLoading}
+          variant="clear"
+          type="submit"
+          className={s.go}
+          icon={GoIcon}
+        />
+      )}
+      {!isFocused && value && (
+        <Button
+          onClick={handleResetInput}
+          iconSize={16}
+          variant="clear"
+          type="button"
+          className={s.go}
+          icon={CrossIcon}
+        />
+      )}
     </form>
   );
 });

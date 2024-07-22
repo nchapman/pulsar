cf4ef9cc6d5753e221f4bcf1eb817415f82f441b
Дмитрий Белоусов <demetrxx@gmail.com>
2024-07-22 10:02:56 +0300

    fix(window-size): fix for windows

--- src/widgets/onboarding/lib/window-size.ts
+++ src/widgets/onboarding/lib/window-size.ts
@@ -23,20 +23,22 @@ async function isWindows() {
 }
 
 export async function minimizeWindowSize() {
-  updateIsResizable();
   appWindow.setMinSize(new LogicalSize(windowSize.onboarding.width, windowSize.onboarding.height));
-  appWindow.setSize(new LogicalSize(windowSize.onboarding.width, windowSize.onboarding.height));
+  await appWindow.setSize(
+    new LogicalSize(windowSize.onboarding.width, windowSize.onboarding.height)
+  );
 
   if (!(await isWindows())) {
     appWindow.setFullscreen(false);
     appWindow.setResizable(false);
   }
+  updateIsResizable();
 }
 
 export async function restoreWindowSize() {
-  updateIsResizable();
   if (window.innerWidth >= windowSize.main.width) return;
-  await appWindow.setMinSize(new LogicalSize(windowSize.main.width, windowSize.main.height));
-  appWindow.setSize(new LogicalSize(windowSize.main.width, windowSize.main.height));
+  appWindow.setMinSize(new LogicalSize(windowSize.main.width, windowSize.main.height));
+  await appWindow.setSize(new LogicalSize(windowSize.main.width, windowSize.main.height));
   appWindow.setResizable(true);
+  updateIsResizable();
 }

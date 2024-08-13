22c8dfd5013a1dea1c055b8a685ada4f0a1d7052
Дмитрий Белоусов <demetrxx@gmail.com>
2024-08-13 12:23:44 +0300

    feat(model-settings): add manual values input

--- src/widgets/model-settings/ui/SliderWithInfo/SliderWithInfo.tsx
+++ src/widgets/model-settings/ui/SliderWithInfo/SliderWithInfo.tsx
@@ -19,29 +19,47 @@ interface Props {
   description: string;
 }
 
-function roundToOne10(value: number) {
-  return Number(value.toFixed(1));
-}
-
 export const SliderWithInfo = memo((props: Props) => {
   const { className, min, max, step, onChange, defaultVal, value, title, description } = props;
 
   const handleChange = (e: any) => {
     const { value } = e.target;
-    if (Number.isNaN(Number(value)) || value === '') {
-      setTimeout(() => onChange(props.value), 100);
-      return;
-    }
-    if ((value * 10) % 1 !== 0) {
-      console.log(Number(value), roundToOne10(Number(value)));
-      setTimeout(() => onChange(roundToOne10(Number(value))), 100);
-      return;
-    }
+    if (value[value.length - 1] === '.') return;
 
-    onChange(Number(value));
+    const numericValue = parseFloat(value);
+
+    if (numericValue < min || numericValue > max) {
+      setTimeout(() => onChange(props.value));
+      return;
+    }
+    onChange(numericValue);
   };
 
   const handleReset = () => onChange(defaultVal);
+
+  const handleKeyPress = (event: any) => {
+    const { value } = event.target;
+
+    // Allow numbers
+    if (/[0-9]/.test(event.key)) {
+      // Allow only one digit after the decimal point
+      const parts = value.split('.');
+      if (parts.length === 2 && parts[1].length >= 1) {
+        event.preventDefault();
+      }
+    } else if (event.key === '.') {
+      // Allow only one decimal point
+      if (step >= 1) {
+        event.preventDefault();
+      }
+
+      if (value.includes('.')) {
+        event.preventDefault();
+      }
+    } else {
+      event.preventDefault();
+    }
+  };
 
   return (
     <div className={classNames(s.sliderWithInfo, [className])}>
@@ -57,7 +75,12 @@ export const SliderWithInfo = memo((props: Props) => {
             iconSize={16}
           />
 
-          <input disabled value={value} className={s.value} type="number" onChange={handleChange} />
+          <input
+            onKeyPress={handleKeyPress}
+            value={value}
+            className={s.value}
+            onChange={handleChange}
+          />
         </div>
       </div>
 

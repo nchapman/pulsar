a780def9dedc63c70a58888888365b4c04d5828e
Дмитрий Белоусов <demetrxx@gmail.com>
2024-08-22 09:11:51 +0300

    fix(model-store-card): remove Plus in the label

--- src/widgets/model-store/ui/ModelCard/ModelCard.tsx
+++ src/widgets/model-store/ui/ModelCard/ModelCard.tsx
@@ -29,7 +29,7 @@ export const ModelCard = memo((props: Props) => {
 
   const stats = [
     {
-      label: `${formatNumber(model.downloadsAllTime)} Plus`,
+      label: `${formatNumber(model.downloadsAllTime)}`,
       icon: DownloadIcon,
     },
     {

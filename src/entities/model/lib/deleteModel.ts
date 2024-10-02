b95ed007b14ec32f24ce73a5d6834119a562a2b0
Дмитрий Белоусов <demetrxx@gmail.com>
2024-10-02 13:00:41 +0300

    feat(models): delete model folders when no files

--- src/entities/model/lib/deleteModel.ts
+++ src/entities/model/lib/deleteModel.ts
@@ -1,6 +1,21 @@
-import { exists, removeFile } from '@tauri-apps/api/fs';
+import { exists, readDir, removeDir, removeFile } from '@tauri-apps/api/fs';
 
-import { getModelPath } from '@/entities/model/lib/getModelPath.ts';
+import { getModelPath } from './getModelPath.ts';
+
+async function deleteEmptyFoldersRecursively(folderPath: string) {
+  if (await exists(folderPath)) {
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const i of await readDir(folderPath)) {
+      if (i.children) {
+        await deleteEmptyFoldersRecursively(i.path);
+      }
+    }
+
+    if ((await readDir(folderPath)).length === 0) {
+      await removeDir(folderPath);
+    }
+  }
+}
 
 export async function deleteModel(d: { fileName: string; model: string }, isDownload = false) {
   try {
@@ -9,6 +24,9 @@ export async function deleteModel(d: { fileName: string; model: string }, isDown
 
     if (fileExists) {
       await removeFile(path);
+      deleteEmptyFoldersRecursively(
+        await getModelPath({ model: d.model.split('/')[0], fileName: '' }, isDownload)
+      );
     }
   } catch (e) {
     console.error('Failed to delete model-file file', e);
